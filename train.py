6614df78b2abb9a5886cf8e26ea878d5b5c440bc
zhuJarvi <158048021+zhujiawei3094@users.noreply.github.com>
2025-09-23 17:00:25 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+!pip install ultralytics
+
+from ultralytics import YOLO
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+model = YOLO(os.path.join(current_dir, "models/9.22/last.pt"))
+
+print("dir right")
+
+model.train(
+    data = "/kaggle/input/military-assets-dataset-12-classes-yolo8-format/military_object_dataset/military_dataset.yaml",
+    epochs = 120,
+    batch = 100,
+    imgsz = 640,
+)
