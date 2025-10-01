d255e46d82851642d3d28f7f4242758d4a3650b2
zhuJarvi <158048021+zhujiawei3094@users.noreply.github.com>
2025-10-01 15:03:36 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-model = YOLO(os.path.join(current_dir, "models/9.22/last.pt"))
+model = YOLO(os.path.join(current_dir, "models/9.29/last.pt"))
 
 print("dir right")
 
@@ -11,5 +11,9 @@ model.train(
     data = "/kaggle/input/military-assets-dataset-12-classes-yolo8-format/military_object_dataset/military_dataset.yaml",
     epochs = 120,
     batch = 64,
-    imgsz = 640,
+    imgsz = 640
 )
+
+model = model.eval()
+
+model.export(format="onnx")
